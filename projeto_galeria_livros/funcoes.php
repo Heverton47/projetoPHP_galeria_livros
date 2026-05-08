e370b3ba97cca84fe5c2091494393ed67b5d3c83
Heverton R.S Santos <hsuchek47@gmail.com>
2026-05-08 16:50:45 -0300

    "alterando funcoes.php"

--- projeto_galeria_livros/funcoes.php
+++ projeto_galeria_livros/funcoes.php
@@ -1,15 +1,16 @@
-<?php foreach($livros as $livro):?>
-<div class="col-12 col-md-4 mb-4">
-<div class="card h-100">
-
-    <div class="card-body" style="display: flex; flex-direction: column; align-items: center; justify-content: center;">
-    <img src="<?= htmlspecialchars($livro['imagem']) ?>" style="height:400px; object-fit:cover;">
-    <h3> <?= htmlspecialchars($livro['titulo']) ?> </h3>
-    <h3> <?= htmlspecialchars($livro['categoria']) ?> </h3>
-    
-    <a href="detalhes.php?id=<?= htmlspecialchars($livro['id']) ?>" class="btn btn-primary btn-lg"> Ver mais </a>
-    </div>
-
-</div>
-</div>
-<?php endforeach; ?>
+<?php
+function mostrarLivros(array $livros): void {
+    foreach ($livros as $livro): ?>
+        <div class="col-12 col-md-4 mb-4">
+            <div class="card h-100">
+                <div class="card-body" style="display: flex; flex-direction: column; align-items: center; justify-content: center;">
+                    <img src="<?= htmlspecialchars($livro['imagem']) ?>" style="height:400px; object-fit:cover;">
+                    <h3><?= htmlspecialchars($livro['titulo']) ?></h3>
+                    <h3><?= htmlspecialchars($livro['categoria']) ?></h3>
+                    <a href="detalhes.php?id=<?= htmlspecialchars($livro['id']) ?>" class="btn btn-primary btn-lg">Ver mais</a>
+                </div>
+            </div>
+        </div>
+    <?php endforeach;
+}
+?>
